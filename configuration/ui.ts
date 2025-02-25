a72d930cefbd1ca7084fa6314d87bb7eca31eb55
Fleming Beaver <108853225+flemingbeaver@users.noreply.github.com>
2025-02-24 23:36:18 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `TorettoBot`;
-export const MESSAGE_PLACEHOLDER: string = ``;
+export const MESSAGE_PLACEHOLDER: string = `Hit the gas`;
 export const FOOTER_MESSAGE: string = `Please don't sue me, Universal and/or Vin Diesel`;
-export const CLEAR_BUTTON_TEXT: string = `Han isn't dead, he's just sleeping`;
-export const PAGE_TITLE: string = `2Chat2Bot`;
-export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI family.`;
+export const CLEAR_BUTTON_TEXT: string = `Retcon`;
+export const PAGE_TITLE: string = `TorettoBot`;
+export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, by ${OWNER_NAME}`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Plot Hole";
